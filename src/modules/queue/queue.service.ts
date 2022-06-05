60e181757242a029f0ad3918db10fe18c93799a5
LiFayi <lifayi2008@gmail.com>
2022-06-05 18:25:03 +0800

    bug fixes

--- src/modules/queue/queue.service.ts
+++ src/modules/queue/queue.service.ts
@@ -18,13 +18,13 @@ export class QueueService {
     this.contractMarket = this.configService.get('CONTRACT_MARKET');
   }
 
-  async onOffSale(tokenId: string, from: string, to: string) {
+  async onOffSale(tokenId: string, from: string, to: string, blockNumber: number) {
     if (to === this.contractMarket) {
+      await this.connection.collection('token_on_order').insertOne({ tokenId, blockNumber });
+    } else if (from === this.contractMarket) {
       await this.connection
         .collection('token_on_order')
-        .updateOne({ tokenId }, { $set: { tokenId } }, { upsert: true });
-    } else if (from === this.contractMarket) {
-      await this.connection.collection('token_on_order').deleteOne({ tokenId });
+        .deleteOne({ tokenId, blockNumber: { $lt: blockNumber } });
     } else {
       this.logger.warn(`Token ${tokenId} is in unknow status`);
     }
--- src/modules/queue/token-order.consumer.ts
+++ src/modules/queue/token-order.consumer.ts
@@ -13,9 +13,17 @@ export class TokenOrderConsumer {
   ) {}
 
   @Process()
-  async transcode(job: Job<{ tokenId: string; from: string; to: string }>) {
+  async transcode(job: Job<{ blockNumber: number; tokenId: string; from: string; to: string }>) {
+    this.logger.log(
+      `Processing job [${this.tokenOnOffSaleQueue.name}] data: ${JSON.stringify(job.data)}`,
+    );
     await this.tokenOnOffSaleQueue.pause();
-    await this.queueService.onOffSale(job.data.tokenId, job.data.from, job.data.to);
+    await this.queueService.onOffSale(
+      job.data.tokenId,
+      job.data.from,
+      job.data.to,
+      job.data.blockNumber,
+    );
     await this.tokenOnOffSaleQueue.resume();
   }
 }
